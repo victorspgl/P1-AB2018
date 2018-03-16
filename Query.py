56c34d55be0d1dab395c86bf7b1131c89bc355cc
Victor Soria <victorspgl@gmail.com>
2018-03-16 13:55:45 +0100

    Creado objeto query para almacenar las consultas

--- Query.py
+++ Query.py
@@ -5,7 +5,8 @@
 class Query:
 
     def __init__(self,string):
-        self.nodo_infectado =
-        self.nodo_consulta =
-        self.timestamp_infeccion =
-        self.timestamp_consulta  =
+        nodo_infectado, nodo_consulta, timestamp_infeccion, timestamp_consulta = [int(i) for i in lineas[0].split(' ')]
+        self.nodo_infectado = nodo_infectado
+        self.nodo_consulta = nodo_consulta
+        self.timestamp_infeccion = timestamp_infeccion
+        self.timestamp_consulta  = timestamp_consulta
