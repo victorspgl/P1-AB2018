711b3a341463f1ad4b72c65b40f3c378feb685e8
Victor Soria <victorspgl@gmail.com>
2018-04-16 09:35:05 +0200

    Mejorado generador de grafos

--- random_graph.py
+++ random_graph.py
@@ -3,21 +3,30 @@
 # 16 Marzo 2018
 
 import random
-from numpy import zeros
+import numpy as np
 
 def random_graph(num_vertices, num_aristas, max_time):
+    if num_aristas > num_vertices*(num_vertices -1):
+        print("Numero de aristas superior a n*(n-1). Siendo n el numero de vertices.")
+        raise Exception
     fichero_objeto = open("random_graph.txt", "w")
     fichero_objeto.write(str(num_vertices) + " " + str(num_aristas) + "\n")
-    matriz_conectados = zeros(num_vertices, num_vertices)
+    matriz_conectados = []
+    for i in range(0,num_vertices):
+        matriz_conectados.append(range(0, num_vertices))
+        matriz_conectados[i].pop(i)
 
     for i in range(0,num_aristas):
-        vertice_inicial = random.randint(0, num_vertices-1)
-        vertice_final = vertice_inicial
-        while(vertice_final == vertice_inicial):
-            vertice_final = random.randint(0, num_vertices-1)
-        if matriz_conectados(vertice_inicial,vertice_final) == 0 and matriz_conectados(vertice_final,vertice_inicial) == 0:
-            matriz_conectados[vertice_inicial, vertice_final] = 1
-            matriz_conectados(vertice_final, vertice_inicial) = 1
+        nok = True
+        while nok:
+            vertice_inicial = random.randint(0, num_vertices - 1)
+            vertice_final = random.randint(0, num_vertices - 1)
+            try:
+                vertice_final = matriz_conectados[vertice_inicial].pop(vertice_final)
+                nok = False
+            except:
+                nok = True
+
 
         timestamp = random.randint(1, max_time)
         fichero_objeto.write(str(vertice_inicial) + " ")
